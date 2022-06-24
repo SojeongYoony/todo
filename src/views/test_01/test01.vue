83a95410fdabe58b5a4d97d67ba5b74de5ea2b8a
SoJeongYoon <y940425@gmail.com>
2022-06-24 21:47:09 +0900

    Create test01.vue

---
+++ src/views/test_01/test01.vue
@@ -0,0 +1,19 @@
+<!-- File > Preference > User Snippets : vue.json에 자주 쓰는 template을 등록하여 불러 올 수 있다. -->
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
